1463bbd23773514b37c2f10c82a6aff0aac20b7f
piligab <pilar@developmentseed.org>
2018-11-21 19:03:16 -0500

    Update file

--- src/items/listItems.js
+++ src/items/listItems.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var request = require('request-promise');
 var config = require('./../config');
 
-module.exports = function (idProject, query) {
+module.exports = function(idProject, query) {
   var url = config.HOST + '/projects/' + idProject + '/items?' + query;
   console.log(url);
   var options = {
@@ -15,19 +15,19 @@ module.exports = function (idProject, query) {
     }
   };
   request(options)
-    .then(function (items) {
+    .then(function(items) {
       var strItems = '';
       for (var i = 0; i < items.length; i++) {
         strItems += JSON.stringify(items[i]) + '\n';
       }
-      fs.writeFile('output.json', strItems, function (err) {
+      fs.writeFile('output.json', strItems, function(err) {
         if (err) {
           return console.log(err);
         }
         console.log('The list was saved in output.json file!');
       });
     })
-    .catch(function (err) {
+    .catch(function(err) {
       console.log(err);
     });
 };
